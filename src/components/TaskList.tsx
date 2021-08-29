85e1d3e4e48063be31f6ca8982132f9afca4d0bc
Ramon Mota <ramon_mota@live.com>
2021-08-29 03:36:08 -0700

    resultado da atividade

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    
+    if (newTaskTitle == "") {
+      console.log("tesk está vazia")
+    } else {
+      setTasks(oldState => [...oldState, newTask]);
+
+      setNewTaskTitle('')
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    console.log("mudar true ou false")
+    const changeIsComplete = tasks.map(task => task.id === id ? 
+      {...task, isComplete: !task.isComplete} : task)
+    
+    setTasks(changeIsComplete);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    console.log("Remover task")
+    const removeTask = tasks.filter(task => task.id !== id);
+
+    setTasks(removeTask);
   }
 
   return (
@@ -66,7 +88,6 @@ export function TaskList() {
               </button>
             </li>
           ))}
-          
         </ul>
       </main>
     </section>
